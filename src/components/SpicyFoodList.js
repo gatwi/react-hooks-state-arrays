8a967b01f1309b863baf6b1c27a7e18525b983c9
gatwi <faith.murithi@student.moringaschool.com>
2022-07-14 14:45:10 +0300

    code along practice

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,11 +3,36 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
     console.log(newFood);
   }
+
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...FormData,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
 
   const foodList = foods.map((food) => (
     <li key={food.id}>
@@ -17,6 +42,13 @@ function SpicyFoodList() {
 
   return (
     <div>
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
